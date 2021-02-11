02571d228ff10480e778b82f3ecbf860171008cd
seankolczynski <sean.kolczynski@gmail.com>
2021-02-11 16:01:20 -0500

    Added ability to include pronouns

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ $(document).ready(function () {
         e.preventDefault();
         // Get values in input fields
         var name = $("input[name='name']").val();
+        var pronouns = $("input[name='pronouns']").val();
+        console.log(pronouns);
         var title = $("input[name='title']").val();
         var email = $("input[name='email']").val();
         // Create signature HTML using ES6 template literal.
         // To update signature, replace this code and template the appropriate variables.
-        var sig = `
+        var mid
+        var front = `
             <br>
             <br>
             <table cellpadding="0" cellspacing="0" border="0" style="background: none; border-width: 0px; border: 0px; margin: 0; padding: 0;">
@@ -19,7 +22,13 @@ $(document).ready(function () {
             <td style="padding-top: 0; padding-bottom: 0; padding-left: 12px; padding-right: 0;">
                 <table cellpadding="0" cellspacing="0" border="0" style="background: none; border-width: 0px; border: 0px; margin: 0; padding: 0;">
                 <tr>
-                    <td colspan="2" style="padding-bottom: 5px; color: #191819; font-weight: bold; font-size: 12px; font-family: 'Playfair Display', sans-serif;">${name}</td></tr>
+                <td colspan="2" style="padding-bottom: 5px; color: #191819; font-weight: bold; font-size: 12px; font-family: 'Playfair Display', sans-serif;">${name}`
+        if (pronouns === "") {
+          mid = `</td></tr>`
+        } else {
+          mid = ` (${pronouns})</td></tr>`
+        }
+        var end = `
                     <tr>
                     <td colspan="2" style="color: #191819; font-size: 12px; font-family: 'Roboto', serif;">${title}</td>
                     </tr>
@@ -29,12 +38,13 @@ $(document).ready(function () {
                 </table>
                 </td>
             </tr>
-            </table>        
-        `;
+            </table>`
+        var sig = front+mid+end;
         // Insert as HTML into page
         $("#output").html(sig);
         // Clear all input fields
         var name = $("input[name='name']").val("");
+        var pronouns = $("input[name='pronouns']").val("");
         var title = $("input[name='title']").val("");
         var email = $("input[name='email']").val("");
     })
